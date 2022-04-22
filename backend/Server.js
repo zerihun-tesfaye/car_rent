a4bc6e43f89d3fba28ba509afdfd447b24641fee
menasehk <menasehk38@gmail.com>
2022-04-23 00:14:24 +0300

    new updates

--- backend/Server.js
+++ backend/Server.js
@@ -1,1 +1,20 @@
-// console.log("hello world");
+import  Express  from "express";
+import cors from "cors";
+import {PORT} from './helpers/constant.js'
+import route from "./route/index.js";
+import {db} from './helpers/connection.js'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = new Express()
+db()
+app.use(cors())
+app.use(Express.urlencoded({extended:false}))
+
+
+// route(app)
+
+
+
+app.listen(PORT | "5000", ()  => {
+    console.log(`App listening on port ${PORT}`);
+});
